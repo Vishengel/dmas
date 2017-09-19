4e417a2213302a1f253d2644ca43a6e8dc46bfc0
Startklaar <paulozk>
2017-09-19 14:21:00 +0200

    Basic GUI, added buttons for controlling the model.

--- Model.py
+++ Model.py
@@ -1,3 +1,10 @@
 class Model():
     def __init__(self):
         pass
+
+    def start_simulation(self):
+        print("Simulation initialized!")
+
+    def play(self, iterations):
+        for i in range(iterations):
+            print("Model is running..")
