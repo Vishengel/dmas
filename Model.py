8b140a1fa9b6926e6906fb5b2858bd181152aae0
Startklaar <paulozk>
2017-10-02 17:58:30 +0200

    Some aspects of the DRBL work :-)
    Widened the screen a bit :-).
    
    Commitment store can now prove a fact, even when a rule has multiple conditions or if multiple rules apply.
    
    Fixed a bug in the prove_conclusion function :-).
    
    Added a function to the Commitment Store class that can check whether a given sentence is provable, given the current
    facts and rules. If it can be proven, the rules that are used for the proof are returned.
    
    Rules now have properties (valid, applicable, apply, excluded).
    
    Forget Pyke. I'm implementing my own very simple logic, which should be sufficient for this project :-). We have Fact, Rule and CS classes. The CS (commitment store) contains a dictionary of Facts and Rules. CS will also get functions for proving whether or not a rule is a reason for a certain conclusion (forward-chaining) or the other way around (backward-chaining). These functions still have to be implemented, though....
    
    Merge branch 'development' of https://github.com/Vishengel/dmas into development
    
    # Conflicts:
    #	Controller.py
    #	Model.py
    
    Added agent method and additional fields.

--- Model.py
+++ Model.py
@@ -6,36 +6,27 @@ from Rule import *
 class Model():
     def __init__(self):
         #Create and init agents
-        self.prosecutor = Agent("Prosecutor", "idle", "exhaustive", CS(), "")
-        self.defendant = Agent("Defendant", "idle", "exhaustive", CS(), "")
+        self.prosecutor = Agent("Prosecutor", "idle", "dumb", CS(), "")
+        self.defendant = Agent("Defendant", "idle", "random", CS(), "")
         self.prosecutor.set_opponent(self.defendant)
         self.defendant.set_opponent(self.prosecutor)
         self.dialogue_stack = []
         self.dialogue_history = []
 
         #Add starting facts
-        first_fact = Fact('borrowed_from', ('defendant', 'prosecutor', 10000), True)
-        #second_fact = Fact('owes', ('defendant', 'prosecutor', 10000), True)
-        second_fact = Fact('hate', ('defendant', 'prosecutor'), False)
-        third_fact = Fact('brothers', ('defendant', 'prosecutor'),False)
-        fourth_fact = Fact('loves', ('defendant','prosecutor'), True)
+        first_fact = Fact('borrowed_from', ('Defendant', 'Prosecutor', 10000), True)
         self.prosecutor.commitment_store.add_fact(first_fact)
-        self.prosecutor.commitment_store.add_fact(second_fact)
-        self.prosecutor.commitment_store.add_fact(third_fact)
-        self.prosecutor.commitment_store.add_fact(fourth_fact)
-
         self.defendant.commitment_store.add_fact(first_fact)
-
-
+        #self.defendant.commitment_store.add_fact( Fact('owes', ('Defendant', 'Prosecutor', 10000), False) )
 
         #Add starting rules
-        first_rule = Rule([Fact('borrowed_from', ('x', 'y', 'amount'), True), Fact('hate', ('x', 'y'), False),
-                           Fact('brothers', ('x', 'y'), False)]
+        starting_rule = Rule([Fact('borrowed_from', ('x', 'y', 'amount'), True)]
                           ,Fact('owes', ('x', 'y', 'amount'), True), 'valid')
-        second_rule = Rule([Fact('loves', ('x', 'y'),True)], Fact('owes', ('x', 'y', 'amount'),True),'valid')
-        self.prosecutor.commitment_store.add_rule(first_rule)
-        self.defendant.commitment_store.add_rule(first_rule)
-        self.prosecutor.commitment_store.add_rule(second_rule)
+        self.prosecutor.commitment_store.add_rule(starting_rule)
+        self.defendant.commitment_store.add_rule(starting_rule)
+
+
+
 
 
 
